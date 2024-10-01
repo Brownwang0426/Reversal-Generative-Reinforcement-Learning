268b396df6ed365ed902adc0eaf0492f0dfd800d
brownwang0426 <brownwang0426@gmail.com>
2024-10-01 22:43:23 +0800

    added new environment

---
+++ envs/env_pendulum.py
@@ -0,0 +1,58 @@
+
+import gym
+
+import numpy as np
+import math
+from scipy.special import softmax
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils.data import DataLoader, TensorDataset, Subset
+
+import csv
+
+import multiprocessing as mp
+import os
+import sys
+import copy
+import random
+import gc
+import time
+from tqdm import tqdm
+
+"""
+# Function for vectorizing
+Crucial function regarding how you manipulate or shape your state, action and reward
+
+- It's essential to choose between immediate rewards and summed rewards for training your agent. 
+  If the current state doesn't encapsulate all crucial past information, using immediate rewards is advisable. 
+  This approach prevents confusion caused by varying summed rewards for the same state.
+
+- As for reward shaping, it is recommended to increase your reward upper and decrease your reward lower bound.
+"""
+
+def quantifying(array_size, min_value, max_value, value):
+    array    = np.zeros(array_size) 
+    interval = (max_value - min_value) / array_size
+    index    = int( (value - min_value) // interval + 1)
+    if index >= 0:
+        array[ : index] = 1
+    return array
+
+def vectorizing_state(state):      # Reminder: change this for your specific task ⚠️⚠️⚠️
+    state_0 = quantifying(100, -1 , 1 , state[0])
+    state_1 = quantifying(100, -1 , 1 , state[1])
+    state_2 = quantifying(100, -8 , 8 , state[2])
+    state   = np.concatenate((state_0, state_1, state_2))
+    return state
+
+def vectorizing_action(pre_activated_actions):  # Reminder: change this for your specific task ⚠️⚠️⚠️
+    activated_actions = torch.sigmoid(pre_activated_actions).cpu().detach().numpy()
+    return activated_actions[0,0], activated_actions[0,0] * 4 - 2
+
+def vectorizing_reward(state, reward, summed_reward, done, reward_size):       # Reminder: change this for your specific task ⚠️⚠️⚠️
+    reward = quantifying(reward_size, -16.2736044, 0, state[0])
+    return reward
