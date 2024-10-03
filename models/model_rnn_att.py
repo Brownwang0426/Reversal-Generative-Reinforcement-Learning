374e2767d9e1ab79190c10bbb3d33512c288b28b
brownwang0426 <brownwang0426@gmail.com>
2024-10-03 10:16:09 +0800

    added rnn_att

---
+++ models/model_rnn_att.py
@@ -0,0 +1,279 @@
+
+import gym
+
+import numpy as np
+import math
+from scipy.special import softmax
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils.data import DataLoader, TensorDataset, Subset
+
+import csv
+
+import multiprocessing as mp
+import os
+import sys
+import copy
+import random
+import gc
+import time
+from tqdm import tqdm
+
+
+
+
+class custom_attn(nn.Module):
+    def __init__(self, d_model, num_heads = 8):
+        super(custom_attn, self).__init__()
+
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+
+        self.bias      = False
+        self.d_model   = d_model
+        self.num_heads = num_heads
+        self.d_k       = d_model // num_heads
+
+        self.W_q  = nn.Linear(d_model, d_model, bias=self.bias)
+        self.W_k  = nn.Linear(d_model, d_model, bias=self.bias)
+        self.W_v  = nn.Linear(d_model, d_model, bias=self.bias)
+        self.W_o  = nn.Linear(d_model, d_model, bias=self.bias)
+
+    def scaled_dot_product_attention(self, Q, K, V, mask=None):
+
+        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / (self.d_k ** 0.5)
+
+        if mask != None:
+            attn_scores += mask
+        else:
+            attn_scores += 0
+
+        attn_probs = torch.softmax(attn_scores, dim=-1)
+        output     = torch.matmul(attn_probs, V)
+
+        return output
+
+    def split_heads(self, x):
+        batch_size, seq_length, d_model = x.size()
+        return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
+        #  (batch_size, seq_length, d_model) - > (batch_size, seq_length, self.num_heads, self.d_k) -> (batch_size, self.num_heads, seq_length, self.d_k)
+
+    def combine_heads(self, x):
+        batch_size, _, seq_length, d_k = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d_model)
+
+    def forward(self, Q, K, V, mask=None):
+        # Q    -> (batch_size, seq_length, d_model)
+        # mask -> (batch_size, 1, seq_length, d_model)
+        Q = self.split_heads(self.W_q(Q))
+        K = self.split_heads(self.W_k(K))
+        V = self.split_heads(self.W_v(V))
+        attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
+        output      = self.W_o(self.combine_heads(attn_output))
+        return output
+
+
+    
+
+class build_model(nn.Module):
+    def __init__(self,
+                 h_input_neuron_size,
+                 hidden_neuron_size,
+                 input_neuron_size,
+                 input_sequence_size,
+                 output_neuron_size,
+                 neural_type,
+                 num_layers,
+                 num_heads,
+                 hidden_activation,
+                 output_activation,
+                 shift,
+                 initializer,
+                 optimizer,
+                 loss,
+                 drop_rate,
+                 alpha,
+                 mask_value):
+
+        super(build_model, self).__init__()
+
+        self.h_input_neuron_size  = h_input_neuron_size
+        self.hidden_neuron_size   = hidden_neuron_size
+        self.input_neuron_size    = input_neuron_size
+        self.input_sequence_size  = input_sequence_size
+        self.output_neuron_size   = output_neuron_size
+        self.neural_type          = neural_type
+        self.num_layers           = num_layers
+        self.num_heads            = num_heads
+        self.hidden_activation    = hidden_activation
+        self.output_activation    = output_activation
+        self.shift                = shift
+        self.initializer          = initializer
+        self.optimizer            = optimizer
+        self.loss                 = loss
+        self.drop_rate            = drop_rate
+        self.alpha                = alpha
+        self.mask_value           = mask_value
+
+        self.bias                 = False
+
+        self.state_linear         = nn.Linear(self.h_input_neuron_size, self.hidden_neuron_size, bias=self.bias)
+        self.action_linear        = nn.Linear(self.input_neuron_size  , self.hidden_neuron_size, bias=self.bias)
+        self.positional_encoding  = nn.Parameter(self.generate_positional_encoding(2, self.hidden_neuron_size ), requires_grad=False)
+        self.transformer_layers   = \
+        nn.ModuleList([
+            nn.ModuleList([
+                custom_attn(self.hidden_neuron_size, self.num_heads),
+                nn.LayerNorm(self.hidden_neuron_size),
+                nn.Linear(self.hidden_neuron_size, self.hidden_neuron_size, bias=self.bias),
+                nn.LayerNorm(self.hidden_neuron_size)
+            ])
+            for _ in range(self.num_layers)
+        ])
+        self.reward_linear        = nn.Linear(self.hidden_neuron_size, self.output_neuron_size , bias=self.bias)
+        self.state_linear_        = nn.Linear(self.hidden_neuron_size, self.h_input_neuron_size, bias=self.bias)
+
+        # Activation functions
+        self.hidden_activation = self.get_activation(self.hidden_activation)
+        self.output_activation = self.get_activation(self.output_activation)
+
+        # Initialize weights for fully connected layers
+        self.initialize_weights(self.initializer  )
+
+        # Optimizer
+        optimizers = {
+            'adam': optim.Adam,
+            'sgd': optim.SGD,
+            'rmsprop': optim.RMSprop
+        }
+        self.selected_optimizer = optimizers[self.optimizer.lower()](self.parameters(), lr=self.alpha)
+
+        # Loss function
+        losses = {
+            'mean_squared_error': torch.nn.MSELoss(),
+            'binary_crossentropy': torch.nn.BCELoss()
+        }
+        self.loss_function = losses[self.loss .lower()]
+
+        # Loss function
+        losses = {
+            'mean_squared_error': torch.nn.MSELoss(reduction='none'),
+            'binary_crossentropy': torch.nn.BCELoss(reduction='none')
+        }
+        self.loss_function_ = losses[self.loss .lower()]
+
+
+    def forward(self, s, a_list):
+        
+        mask = None
+
+        r_list = list()
+        s_list = list()
+
+        s  = self.state_linear(s)
+        s  = self.hidden_activation(s)
+
+        a_ = self.action_linear(a_list[:,0])
+        a_ = self.hidden_activation(a_)
+
+        h  = torch.stack([s, a_], dim=0).view(a_.size(0), 2, a_.size(1))
+        h  = h + self.positional_encoding[:, :, :]
+
+        pres_h_list = list()
+        for j, layer in enumerate(self.transformer_layers):
+            attention_layer, attention_norm_layer, fully_connected_layer, fully_connected_norm_layer = layer
+            h_ = attention_layer(h, h, h, mask)
+            h  = attention_norm_layer(h + h_)
+            h_ = fully_connected_layer(h)
+            h  = fully_connected_norm_layer(h + h_)
+            pres_h_list.append(h)
+        prev_h_list = pres_h_list
+
+        r  = h[:, 0]
+        s_ = h[:, 1]
+        
+        r  = self.reward_linear(r)   
+        r  = self.output_activation(r)
+
+        s_ = self.state_linear_(s_)   
+        s_ = self.output_activation(s_)
+
+        s  = self.state_linear(s_)
+        s  = self.hidden_activation(s)
+
+        r_list.append(r)
+        s_list.append(s_)
+
+        for i in range(a_list.size(1)-1):
+
+            a_ = self.action_linear(a_list[:,i+1])
+            a_ = self.hidden_activation(a_)
+
+            h  = torch.stack([s, a_], dim=0).view(a_.size(0), 2, a_.size(1))
+            h  = h + self.positional_encoding[:, :, :]
+
+            pres_h_list = list()
+            for j, layer in enumerate(self.transformer_layers):
+                attention_layer, attention_norm_layer, fully_connected_layer, fully_connected_norm_layer = layer
+                h_ = attention_layer(prev_h_list[j], prev_h_list[j], h, mask)
+                h  = attention_norm_layer(h + h_)
+                h_ = fully_connected_layer(h)
+                h  = fully_connected_norm_layer(h + h_)
+                pres_h_list.append(h)
+            prev_h_list = pres_h_list
+
+            r  = h[:, 0]
+            s_ = h[:, 1]
+            
+            r  = self.reward_linear(r)   
+            r  = self.output_activation(r)
+
+            s_ = self.state_linear_(s_)   
+            s_ = self.output_activation(s_)
+
+            s  = self.state_linear(s_)
+            s  = self.hidden_activation(s)
+
+            r_list.append(r)
+            s_list.append(s_)
+
+        r_list = torch.stack(r_list, dim=1)
+        s_list = torch.stack(s_list, dim=1)
+        
+        return r_list, s_list
+
+
+    def generate_positional_encoding(self, max_len, model_dim):
+        pe = torch.zeros(max_len,model_dim)
+        for pos in range(max_len):
+            for i in range(0,model_dim,2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/model_dim)))
+                if i + 1 < model_dim:
+                    pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * i)/model_dim)))
+        return pe.unsqueeze(0)  # Shape: (1, max_len, model_dim)
+
+    def get_activation(self,  activation):
+        activations = {
+            'relu': nn.ReLU(),
+            'leaky_relu': nn.LeakyReLU(),
+            'sigmoid': nn.Sigmoid(),
+            'tanh': nn.Tanh()
+        }
+        return activations[ activation.lower()]
+
+    def initialize_weights(self, initializer):
+        initializers = {
+            'random_uniform': nn.init.uniform_,
+            'random_normal': nn.init.normal_,
+            'glorot_uniform': nn.init.xavier_uniform_,
+            'glorot_normal': nn.init.xavier_normal_,
+            'xavier_uniform': nn.init.xavier_uniform_,
+            'xavier_normal': nn.init.xavier_normal_
+        }
+        initializer = initializers[initializer.lower()]
+        for layer in self.children():
+            if isinstance(layer, nn.Linear):
+                initializer(layer.weight)
---
+++ utils/util_rnn_att.py
@@ -0,0 +1,236 @@
+
+import gym
+
+import numpy as np
+import math
+from scipy.special import softmax
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils.data import DataLoader, TensorDataset, Subset
+
+import csv
+
+import multiprocessing as mp
+import os
+import sys
+import copy
+import random
+import gc
+import time
+from tqdm import tqdm
+
+
+
+def update_pre_activated_actions(epoch_for_deducing,
+                                 model_list,
+                                 state,
+                                 pre_activated_future_action,
+                                 desired_future_reward,
+                                 beta,
+                                 device):
+    
+    desired_future_reward = torch.ones((1, pre_activated_future_action.size(1), desired_future_reward.size(1)))
+
+    state, pre_activated_future_action, desired_future_reward = state.to(device), pre_activated_future_action.to(device), desired_future_reward.to(device)
+
+    model_list_copy = copy.deepcopy(model_list)
+
+    for epoch in range(epoch_for_deducing):
+
+        random.shuffle(model_list_copy)
+
+        for model in model_list_copy:
+
+            future_action = torch.sigmoid(pre_activated_future_action)
+
+            model.train()
+            future_action = future_action.clone().detach().requires_grad_(True)
+            if future_action.grad is not None:
+                future_action.grad.zero_()
+            for param in model.parameters():
+                param.requires_grad = False
+
+            loss_function       = model.loss_function
+            output_reward, _    = model(state, future_action)
+            total_loss          = loss_function(output_reward, desired_future_reward)
+            total_loss.backward() # get grad
+
+            pre_activated_future_action -= future_action.grad * (1 - future_action) * future_action * beta # update params
+
+    return pre_activated_future_action
+
+
+
+
+# traditional EWC
+def EWC_loss(EWC_lambda, model, prev_model, prev_gradient_matrix):
+    model_param      = model.state_dict()
+    prev_model_param = prev_model.state_dict()
+    loss = 0
+    for name, param in model.named_parameters():
+        diagonal_fisher_matrix = prev_gradient_matrix[name] ** 2
+        param_diff             = (model_param[name] - prev_model_param[name]) ** 2
+        loss                  += (diagonal_fisher_matrix * param_diff).sum()
+    return EWC_lambda * loss
+
+
+
+
+def update_model(model,
+                 sub_data_loader,
+                 prev_model,
+                 prev_gradient_matrix,
+                 EWC_lambda):
+
+    for state, future_action, future_reward, future_state, padding_mask in sub_data_loader:
+
+        model.train()
+        selected_optimizer = model.selected_optimizer
+        selected_optimizer.zero_grad()
+
+        loss_function               = model.loss_function
+        output_reward, output_state = model(state, future_action)
+        total_loss                  = loss_function(output_reward, future_reward) + loss_function(output_state, future_state)
+        total_loss                 += EWC_loss(EWC_lambda, model, prev_model, prev_gradient_matrix)
+        total_loss.backward()     # get grad
+
+        selected_optimizer.step() # update params
+
+    return model
+
+
+
+
+def update_gradient_matrix(model,
+                           data_loader):
+    
+    gradient_matrix = {name: torch.zeros_like(param) for name, param in model.named_parameters()}
+
+    for state, future_action, future_reward, future_state, padding_mask in data_loader:
+
+        model.train()
+        selected_optimizer = model.selected_optimizer
+        selected_optimizer.zero_grad()
+
+        loss_function               = model.loss_function
+        output_reward, output_state = model(state, future_action)
+        total_loss                  = loss_function(output_reward, future_reward) 
+        total_loss.backward()        # get grad
+
+        for name, param in model.named_parameters():
+            if name != "positional_encoding":
+                gradient_matrix[name] += param.grad
+
+    gradient_matrix = {name: param / len(data_loader) for name, param in gradient_matrix.items()}
+
+    return gradient_matrix
+
+
+
+
+def initialize_pre_activated_actions(init, noise_t, noise_r, shape):
+    input = 0
+    if   init == "random_uniform":
+        for _ in range(noise_t):
+            input += np.random.uniform(low=0, high=1, size=shape) * noise_r
+    elif init == "random_normal":
+        for _ in range(noise_t):
+            input += np.random.normal(loc=0.0, scale= 1, size= shape ) * noise_r
+    elif init == "glorot_uniform":
+        for _ in range(noise_t):
+            limit = np.sqrt(6 / (shape[1] + shape[1]))
+            input += np.random.uniform(low=-limit, high=limit, size=shape) * noise_r
+    elif init == "glorot_normal":
+        for _ in range(noise_t):
+            input += np.random.normal(loc=0.0, scale= np.sqrt(2 / (shape[1] + shape[1])) , size= shape ) * noise_r
+    elif init == "xavier_uniform":
+        for _ in range(noise_t):
+            limit = np.sqrt(6 / (shape[1] + shape[1]))
+            input += np.random.uniform(low=-limit, high=limit, size=shape) * noise_r
+    elif init == "xavier_normal":
+        for _ in range(noise_t):
+            input += np.random.normal(loc=0.0, scale= np.sqrt(2 / (shape[1] + shape[1])) , size= shape ) * noise_r
+    return input
+
+
+
+
+def sequentialize(short_term_state_list, short_term_action_list, short_term_reward_list, time_size):
+
+    time_size = 5
+
+    short_term_present_state_list = []
+    short_term_future_action_list = []
+    short_term_future_reward_list = []
+    short_term_future_state_list  = []
+
+    if time_size > len(short_term_state_list[:-1]):
+        time_size = len(short_term_state_list[:-1])
+    else:
+      pass
+
+    for i in range(len(short_term_reward_list[:-time_size+1])):
+        short_term_present_state_list.append(      short_term_state_list [ i                       ]  )
+        short_term_future_action_list.append(      short_term_action_list[ i   : i+time_size       ]  )
+        short_term_future_reward_list.append(      short_term_reward_list[ i   : i+time_size       ]  )
+        short_term_future_state_list.append(       short_term_state_list [ i+1 : i+time_size+1     ]  )
+
+    return short_term_present_state_list, short_term_future_action_list, short_term_future_reward_list, short_term_future_state_list
+
+
+
+
+def obtain_tensor_from_list(short_term_state_list,
+                            short_term_future_action_list,
+                            short_term_future_reward_list,
+                            short_term_future_state_list,
+                            time_size,
+                            mask_value,
+                            num_heads,
+                            device):
+
+    # Convert lists to tensors directly on the desired device and data type
+    short_term_state_tensor         = torch.tensor(np.array(short_term_state_list), dtype=torch.float).to(device)
+    short_term_future_action_tensor = torch.tensor(np.array(short_term_future_action_list), dtype=torch.float).to(device)
+    short_term_future_reward_tensor = torch.tensor(np.array(short_term_future_reward_list), dtype=torch.float).to(device)
+    short_term_future_state_tensor  = torch.tensor(np.array(short_term_future_state_list), dtype=torch.float).to(device)
+
+    return short_term_state_tensor, short_term_future_action_tensor, short_term_future_reward_tensor, short_term_future_state_tensor, None
+
+
+
+
+def obtain_TD_error(model,
+                    data_loader):
+
+    for state, future_action, future_reward, future_state in data_loader:
+
+        model.train()
+        selected_optimizer = model.selected_optimizer
+        selected_optimizer.zero_grad()
+
+        loss_function        = model.loss_function_
+        output_reward, _     = model(state, future_action)
+        total_loss           = loss_function(output_reward, future_reward).detach()
+        total_loss           = torch.sum(torch.abs(total_loss), dim=(1, 2))
+
+    return total_loss
+
+
+
+
+def save_performance_to_csv(performance_log, filename='performance_log.csv'):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Episode', 'Summed_Reward'])
+        writer.writerows(performance_log)
+
+
+
+
+
+
