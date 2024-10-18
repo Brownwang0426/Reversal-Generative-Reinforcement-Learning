ff7cc11cb430470858947c7dc00a785d0e86e3fa
Po-Lung Wang <Brownwang0426@gmail.com>
2024-10-18 14:29:38 +0800

    updated TD error function
    updated TD error function

--- utils/util_rnn.py
+++ utils/util_rnn.py
@@ -195,9 +195,11 @@ def obtain_TD_error(model,
 
         loss_function                 = model.loss_function_
         output_reward, output_state   = model(state, future_action)
-        total_loss                    = loss_function(output_reward[:, -1, :], future_reward[:, -1, :]).detach()
-        total_loss                    = torch.sum(torch.abs(total_loss), dim=1) 
-        TD_error                      = total_loss
+        total_loss_1                  = loss_function(output_reward, future_reward).detach()
+        total_loss_1                  = torch.sum(torch.abs(total_loss_1), dim=(1, 2)) / future_reward.size(2)
+        total_loss_2                  = loss_function(output_state, future_state).detach()
+        total_loss_2                  = torch.sum(torch.abs(total_loss_2), dim=(1, 2)) / future_state.size(2)
+        TD_error                      = total_loss_1 + total_loss_2 
         
     return TD_error 
 
