4ca6b21320ca3c3b434f6bc3bf2f9ccf1e514ee5
Po-Lung Wang <Brownwang0426@gmail.com>
2024-12-07 18:27:01 +0800

    Add files via upload

--- models/model_rnn.py
+++ models/model_rnn.py
@@ -83,14 +83,10 @@ class build_model(nn.Module):
             'lstm': nn.LSTM
         }
 
-        self.fully_connected_layer_in_0      = nn.Linear(self.h_input_neuron_size, self.hidden_neuron_size, bias=self.bias)
-        self.fully_connected_layer_in_1      = nn.Linear(self.hidden_neuron_size , self.hidden_neuron_size, bias=self.bias)
-        self.fully_connected_layer_out_0     = nn.Linear(self.hidden_neuron_size , self.hidden_neuron_size, bias=self.bias)
-        self.fully_connected_layer_out_1     = nn.Linear(self.hidden_neuron_size , self.h_input_neuron_size, bias=self.bias)
-
-        self.recurrent_layer_0               = neural_types[neural_type.lower()](self.input_neuron_size, self.hidden_neuron_size, num_layers= self.num_layers, batch_first=False, bias=self.bias, dropout=self.drop_rate)
-
-        self.fully_connected_layer_r         = nn.Linear(self.hidden_neuron_size * self.input_sequence_size, self.output_neuron_size, bias=self.bias)
+        self.recurrent_layer_1    = neural_types[self.neural_type.lower()](self.input_neuron_size, self.h_input_neuron_size, num_layers=self.num_layers, batch_first=True, bias=self.bias, dropout=self.drop_rate)
+        self.recurrent_layer_2    = neural_types[self.neural_type.lower()](self.input_neuron_size, self.h_input_neuron_size, num_layers=self.num_layers, batch_first=True, bias=self.bias, dropout=self.drop_rate)
+        self.recurrent_layer_3    = neural_types[self.neural_type.lower()](self.input_neuron_size, self.h_input_neuron_size, num_layers=self.num_layers, batch_first=True, bias=self.bias, dropout=self.drop_rate)
+        self.reward_linear        = nn.Linear(self.h_input_neuron_size, self.output_neuron_size, bias=self.bias)
 
         # Activation functions
         self.hidden_activation    = self.get_activation(self.hidden_activation)
@@ -123,49 +119,51 @@ class build_model(nn.Module):
 
 
 
-
-    def forward(self, s, a, mask):
-        
+    def forward(self, s, a_list):
+        null_step = torch.zeros_like(a_list[:, 0, :]).unsqueeze(1)
         idx = 0 # the index of the num_layers where you want to insert s
-
-        s  = self.fully_connected_layer_in_0(s)
-        s  = self.hidden_activation(s)
-        s  = self.fully_connected_layer_in_1(s)
-        s  = self.hidden_activation(s)
-
-        a        = a.permute(1, 0, 2)
-        lengths    = (a != self.mask_value).any(dim=2).sum(dim=0).cpu().long() # since x is (sequence_length, batch_size, input_size), we should use sum(dim=0)
-        a        = rnn_utils.pack_padded_sequence(a, lengths, batch_first=False, enforce_sorted=False)
-
-        if self.neural_type == 'lstm':
-            c       = torch.zeros_like(s).repeat(self.num_layers, 1, 1) - 1
-            h       = torch.zeros_like(s).repeat(self.num_layers, 1, 1) - 1        
-            h[idx]  = s  
-            r, hc     = self.recurrent_layer_0(a, (h, c))
-            h          = hc[0] 
-            s     = h[idx]
-        else:
-            h       = torch.zeros_like(s).repeat(self.num_layers, 1, 1) - 1        
-            h[idx]  = s  
-            r, h     = self.recurrent_layer_0(a, h)
-            s     = h[idx]
-            
-        r, _     = rnn_utils.pad_packed_sequence(r, batch_first=False)
-        padding    = (0, 0, 0, 0, 0, self.input_sequence_size - r.size(0))
-        r        = F.pad(r, padding, "constant", 0)
-        r        = r.permute(1, 0, 2)
-
-        s  = self.fully_connected_layer_out_0(s)
-        s  = self.hidden_activation(s)
-        s  = self.fully_connected_layer_out_1(s)
-        s  = self.output_activation(s)
-
-        r = torch.flatten(r, start_dim=1)
-
-        r = self.fully_connected_layer_r(r)
-        r = self.output_activation(r)
-
-        return r, s
+        # s          is [batch_size, feature_size] by default
+        # a_list     is [batch_size, sequence_size, feature_size] by default
+        # cl         is [num_layers, batch_size, feature_size]
+        # sl         is [num_layers, batch_size, feature_size]
+        # rl         is [batch_size, sequence_size, feature_size] 
+        r_list = list()
+        s_list = list()
+        for i in range(a_list.size(1)):
+            if self.neural_type == 'lstm':
+                if i == 0:
+                    cl       = torch.zeros_like(s).repeat(self.num_layers, 1, 1) - 1
+                    sl       = torch.zeros_like(s).repeat(self.num_layers, 1, 1) - 1        
+                    sl[idx]  = s  
+                else:
+                    pass                             
+                rl, (sl, cl) = self.recurrent_layer_1(null_step                     , (sl, cl))
+                rl, (sl, cl) = self.recurrent_layer_2(a_list[:, i, :].unsqueeze(1)  , (sl, cl))
+                r            = rl[:,0,:] 
+                rl, (sl, cl) = self.recurrent_layer_3(null_step                     , (sl, cl))
+                s            = sl[idx]
+                c            = cl[idx]
+            else:
+                if i == 0:
+                    sl       = torch.zeros_like(s).repeat(self.num_layers, 1, 1) - 1        
+                    sl[idx]  = s                                                                         
+                else:
+                    pass
+                rl, sl       = self.recurrent_layer_1(null_step                     , sl)
+                rl, sl       = self.recurrent_layer_2(a_list[:, i, :].unsqueeze(1)  , sl)
+                r            = rl[:,0,:]
+                rl, sl       = self.recurrent_layer_3(null_step                     , sl)    
+                s            = sl[idx]
+                
+            r  = self.reward_linear(r)   
+            r  = self.output_activation(r)
+            r_list.append(r) # r_list is [sequence_size, batch_size, feature_size]
+            s_list.append(s) # s_list is [sequence_size, batch_size, feature_size]
+        r_list = torch.stack(r_list, dim=0) # r_list becomes [sequence_size, batch_size, feature_size]
+        s_list = torch.stack(s_list, dim=0) # s_list becomes [sequence_size, batch_size, feature_size]
+        r_list = r_list.permute(1, 0, 2)    # r_list becomes [batch_size, sequence_size, feature_size]
+        s_list = s_list.permute(1, 0, 2)    # s_list becomes [batch_size, sequence_size, feature_size]
+        return r_list, s_list
 
 
 
