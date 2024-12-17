fa6c089b045c1cb5fd0fd79f9ed42b3c45014df9
Po-Lung Wang <Brownwang0426@gmail.com>
2024-12-17 18:17:46 +0800

    Update env_mountaincar.py

--- envs/env_mountaincar.py
+++ envs/env_mountaincar.py
@@ -50,7 +50,7 @@ def vectorizing_state(state, device):      # Reminder: change this for your spec
     state   = torch.cat((state_0, state_1), dim = 0)
     return state
 
-def vectorizing_action(pre_activated_actions):  # Reminder: change this for your specific task ⚠️⚠️⚠️
+def vectorizing_action(pre_activated_actions, device):  # Reminder: change this for your specific task ⚠️⚠️⚠️
     action_size       = pre_activated_actions.size(2)
     action_argmax     = int(torch.argmax(pre_activated_actions[0, 0]))
     vectorized_action = torch.eye(action_size)[action_argmax].to(device)
