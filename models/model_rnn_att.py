630bedfeaded4770e9d860a3016b99a3df84b443
Assist.moli <assist.moli@gmail.com>
2025-01-19 06:45:50 +0800

    fixed rnn_att, etc.

--- models/model_rnn_att.py
+++ models/model_rnn_att.py
@@ -29,50 +29,42 @@ import itertools
 
 
 class custom_attn(nn.Module):
-    def __init__(self, d_model, num_heads = 8):
+    def __init__(self, feature_size, num_heads):
         super(custom_attn, self).__init__()
-
-        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
-
-        self.bias      = False
-        self.d_model   = d_model
-        self.num_heads = num_heads
-        self.d_k       = d_model // num_heads
-
-        self.W_q  = nn.Linear(d_model, d_model, bias=self.bias)
-        self.W_k  = nn.Linear(d_model, d_model, bias=self.bias)
-        self.W_v  = nn.Linear(d_model, d_model, bias=self.bias)
-        self.W_o  = nn.Linear(d_model, d_model, bias=self.bias)
-
-    def scaled_dot_product_attention(self, Q, K, V, mask=None):
-
-        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / (self.d_k ** 0.5)
-
-        if mask != None:
-            attn_scores += mask
-        else:
-            attn_scores += 0
-
-        attn_probs = torch.softmax(attn_scores, dim=-1)
-        output     = torch.matmul(attn_probs, V)
-
-        return output
+        assert feature_size % num_heads == 0, "feature_size must be divisible by num_heads"
+        self.bias          = False
+        self.feature_size  = feature_size
+        self.num_heads     = num_heads
+        self.head_size     = feature_size // num_heads
+        self.W_q           = nn.Linear(feature_size, feature_size, bias=self.bias)
+        self.W_k           = nn.Linear(feature_size, feature_size, bias=self.bias)
+        self.W_v           = nn.Linear(feature_size, feature_size, bias=self.bias)
+        self.W_o           = nn.Linear(feature_size, feature_size, bias=self.bias)
 
     def split_heads(self, x):
-        batch_size, seq_length, d_model = x.size()
-        return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
-        #  (batch_size, seq_length, d_model) - > (batch_size, seq_length, self.num_heads, self.d_k) -> (batch_size, self.num_heads, seq_length, self.d_k)
+        batch_size, sequence_size, feature_size = x.size()
+        return x.view(batch_size, sequence_size, self.num_heads, self.head_size).transpose(1, 2)
+
+    def scaled_dot_product_attention(self, Q, K, V, mask):
+        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / (self.head_size ** 0.5) #  (batch_size, num_heads, sequence_size, head_size) @ (batch_size, num_heads, head_size, sequence_size ) 
+        if mask != None:
+            attn_scores += mask                   # (batch_size, num_heads, sequence_size, sequence_size) += (batch_size, 1, sequence_size, sequence_size)
+        else:
+            attn_scores += 0
+        attn_probs = torch.softmax(attn_scores, dim=-1)
+        output     = torch.matmul(attn_probs, V)  # (batch_size, num_heads, sequence_size, sequence_size) @ (batch_size, num_heads, sequence_size, head_size ) 
+        return output                             # (batch_size, num_heads, sequence_size, head_size)
 
     def combine_heads(self, x):
-        batch_size, _, seq_length, d_k = x.size()
-        return x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d_model)
+        batch_size, num_heads, sequence_size, head_size = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, sequence_size, self.feature_size)
 
     def forward(self, Q, K, V, mask=None):
-        # Q    -> (batch_size, seq_length, d_model)
-        # mask -> (batch_size, 1, seq_length, d_model)
-        Q = self.split_heads(self.W_q(Q))
-        K = self.split_heads(self.W_k(K))
-        V = self.split_heads(self.W_v(V))
+        # mask Shape: (batch_size, 1, sequence_size, sequence_size)
+        # Q    Shape: (batch_size,    sequence_size, feature_size )
+        Q    = self.split_heads(self.W_q(Q))  # Shape: (batch_size, num_heads, sequence_size, head_size )
+        K    = self.split_heads(self.W_k(K))  # Shape: (batch_size, num_heads, sequence_size, head_size )
+        V    = self.split_heads(self.W_v(V))  # Shape: (batch_size, num_heads, sequence_size, head_size )
         attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
         output      = self.W_o(self.combine_heads(attn_output))
         return output
@@ -119,7 +111,6 @@ class build_model(nn.Module):
         self.bias                 = bias
         self.drop_rate            = drop_rate
         self.alpha                = alpha
-
 
         self.state_linear         = nn.Linear(self.h_input_neuron_size, self.hidden_neuron_size, bias=self.bias)
         self.action_linear        = nn.Linear(self.input_neuron_size  , self.hidden_neuron_size, bias=self.bias)
@@ -217,19 +208,18 @@ class build_model(nn.Module):
         s_list = torch.stack(s_list, dim=0) # s_list becomes [sequence_size, batch_size, feature_size]
         r_list = r_list.permute(1, 0, 2)    # r_list becomes [batch_size, sequence_size, feature_size]
         s_list = s_list.permute(1, 0, 2)    # s_list becomes [batch_size, sequence_size, feature_size]
-        s_list = s_list.repeat(1, self.num_layers, 1, 1)    
         
         return r_list, s_list
 
 
-    def generate_positional_encoding(self, max_len, model_dim):
-        pe = torch.zeros(max_len,model_dim)
-        for pos in range(max_len):
-            for i in range(0,model_dim,2):
-                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/model_dim)))
-                if i + 1 < model_dim:
-                    pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * i)/model_dim)))
-        return pe.unsqueeze(0)  # Shape: (1, max_len, model_dim)
+    def generate_positional_encoding(self, sequence_size, feature_size):
+        pe = torch.zeros(sequence_size,feature_size)
+        for pos in range(sequence_size):
+            for i in range(0,feature_size,2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/feature_size)))
+                if i + 1 < feature_size:
+                    pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * i)/feature_size)))
+        return pe.unsqueeze(0)  # Shape: (1, sequence_size, feature_size)
 
     def get_activation(self,  activation):
         activations = {
