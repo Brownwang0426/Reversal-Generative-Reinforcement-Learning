34f4930066ac2f008135b12cdc4031cf2dbda969
Po-Lung Wang <Brownwang0426@gmail.com>
2025-04-30 09:29:19 +0800

    Add files via upload

--- utils/util_func.py
+++ utils/util_func.py
@@ -271,7 +271,7 @@ def obtain_obsolute_TD_error(model,
 
     return TD_error
 
-def update_model(iteration_for_learning,
+def update_model_(iteration_for_learning,
                  history_state_stack,
                  history_action_stack,
                  present_state_stack,
@@ -283,12 +283,9 @@ def update_model(iteration_for_learning,
 
     PER_epsilon     = 1e-20
     PER_exponent    = 2
-    PER_replacement = True
-    if PER_replacement != True:
-        batch_size  = min(batch_size, len(present_state_stack))
-    else:
-        pass
-
+    PER_replacement = False
+    batch_size      = min(batch_size, len(present_state_stack))
+    
     obsolute_TD_error    = obtain_obsolute_TD_error(model, 
                                                     history_state_stack  ,
                                                     history_action_stack ,
@@ -334,7 +331,47 @@ def update_model(iteration_for_learning,
         priority_probability        = exponent_priority / torch.sum(exponent_priority)
 
     return model
-    
+
+
+
+
+def update_model(iteration_for_learning,
+                 history_state_stack,
+                 history_action_stack,
+                 present_state_stack,
+                 future_action_stack,
+                 future_reward_stack,
+                 future_state_stack ,
+                 model,
+                 batch_size):
+
+    n           = len(present_state_stack)
+    probability = torch.full((n,), 1.0 / n)
+
+    for _ in tqdm(range(iteration_for_learning)):
+
+        indices        = torch.multinomial(probability, batch_size, replacement = False)
+        history_state  = history_state_stack [indices]
+        history_action = history_action_stack[indices]
+        present_state  = present_state_stack [indices]
+        future_action  = future_action_stack [indices]
+        future_reward  = future_reward_stack [indices]
+        future_state   = future_state_stack  [indices]
+
+        model.train()
+        selected_optimizer = model.selected_optimizer
+        selected_optimizer.zero_grad()
+
+        loss_function               = model.loss_function
+        envisaged_reward, \
+        envisaged_state             = model(history_state, history_action, present_state, future_action)
+        total_loss                  = loss_function(envisaged_reward, future_reward) + loss_function(envisaged_state, future_state )
+        total_loss.backward()     
+
+        selected_optimizer.step() 
+
+    return model
+
 def update_model_list(iteration_for_learning,
                       history_state_stack,
                       history_action_stack,
