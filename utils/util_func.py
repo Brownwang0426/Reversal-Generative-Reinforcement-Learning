df096b8220f47d143c34fedf403470b2c7ebeff5
Po-Lung Wang <Brownwang0426@gmail.com>
2025-06-09 10:32:56 +0800

    Add files via upload

--- utils/util_func.py
+++ utils/util_func.py
@@ -368,7 +368,10 @@ def update_model_(itrtn_for_learning,
         subset_loader        = DataLoader(subset, batch_size=batch_size, shuffle=False)
         for history_state, history_action, present_state, future_action, future_reward, future_state in subset_loader:
 
-            history_len      = np.random.randint(0, history_state.size(1))
+            if history_state.size(1) > 0:
+                history_len = np.random.randint(0, history_state.size(1))
+            else:
+                history_len = 0  
             history_state    = history_state [:, -history_len:,:]
             history_action   = history_action[:, -history_len:,:]
 
@@ -403,7 +406,10 @@ def update_model(itrtn_for_learning,
         subset_loader  = DataLoader(subset, batch_size=batch_size, shuffle=False)
         for history_state, history_action, present_state, future_action, future_reward, future_state in subset_loader:
 
-            history_len      = np.random.randint(0, history_state.size(1))
+            if history_state.size(1) > 0:
+                history_len = np.random.randint(0, history_state.size(1))
+            else:
+                history_len = 0  
             history_state    = history_state [:, -history_len:,:]
             history_action   = history_action[:, -history_len:,:]
 
@@ -438,7 +444,10 @@ def update_model_(epoch_for_learning,
 
         for history_state, history_action, present_state, future_action, future_reward, future_state in data_loader:
             
-            history_len      = np.random.randint(0, history_state.size(1))
+            if history_state.size(1) > 0:
+                history_len = np.random.randint(0, history_state.size(1))
+            else:
+                history_len = 0  
             history_state    = history_state [:, -history_len:,:]
             history_action   = history_action[:, -history_len:,:]
 
