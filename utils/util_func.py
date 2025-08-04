f314dfe6fc7e869a0acdfa23319b3db45e40fa7d
Po-Lung Wang <Brownwang0426@gmail.com>
2025-08-04 17:03:57 +0800

    Add files via upload

--- utils/util_func.py
+++ utils/util_func.py
@@ -121,7 +121,7 @@ def update_future_action(itrtn_for_planning,
         loss_function      = model.loss_function
         envisaged_reward, \
         envisaged_state    = model(history_state, history_action, present_state, future_action_)
-        total_loss         = loss_function(envisaged_reward, desired_reward)
+        total_loss         = loss_function(envisaged_reward[:, -1, :], desired_reward[:, -1, :])
         total_loss.backward() 
 
         future_action     -= future_action_.grad * (1 - future_action_ * future_action_) * beta 
@@ -159,7 +159,7 @@ def update_future_action_(epoch_for_planning,
             loss_function      = model.loss_function
             envisaged_reward, \
             envisaged_state    = model(history_state, history_action, present_state, future_action_)
-            total_loss         = loss_function(envisaged_reward, desired_reward)
+            total_loss         = loss_function(envisaged_reward[:, -1, :], desired_reward[:, -1, :])
             total_loss.backward() 
 
             future_action     -= future_action_.grad * (1 - future_action_ * future_action_) * beta 
