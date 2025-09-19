a4a71a0c095d3b7dfbf150b2382bb5e132798361
Po-Lung Wang <Brownwang0426@gmail.com>
2025-09-19 20:14:08 +0800

    ww

--- utils/util_func.py
+++ utils/util_func.py
@@ -340,7 +340,7 @@ def obtain_obsolute_TD_error(model, dataset, td_error_batch, device):
 
     return TD_error
 
-def update_model_(itrtn_for_learning,
+def update_model(itrtn_for_learning,
                  dataset,
                  model,
                  batch_size):
@@ -400,7 +400,7 @@ def update_model_(itrtn_for_learning,
 
 
 
-def update_model(itrtn_for_learning,
+def update_model_(itrtn_for_learning,
                  dataset,
                  model,
                  batch_size):
