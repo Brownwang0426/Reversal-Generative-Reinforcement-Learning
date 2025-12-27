7396b5412e8e6b25b643f209def142183180951b
Po-Lung Wang <Brownwang0426@gmail.com>
2025-12-27 15:57:32 +0800

    w

--- main.py
+++ main.py
@@ -211,8 +211,49 @@ render_for_human = False
 
 
 
+game_name =  'MountainCar-v0'        #⚠️
+max_steps_for_each_episode = 200     #⚠️
+seed = None                          #⚠️
+load_pretrained_model = True
+ensemble_size = 10                   #◀️
+state_size =  220                    #⚠️
+action_size = 3                      #⚠️
+reward_size = 100                    #⚠️
+feature_size = 300                   #⚠️
+history_size  = 200                  #⚠️
+future_size = 200                    #⚠️
+skip = 10                            #⚠️ 
+neural_type = 'td'                   #⚠️
+num_layers = 3                       #⚠️
+num_heads = 10                       #⚠️
 
+init = "xavier_normal"
+opti = 'sgd'
+loss = 'mean_squared_error'
+bias = False
+drop_rate = 0.001
+alpha = 0.1
+L2_lambda = 0                 
+grad_clip_value = 1.0
+min_itrtn_for_learning = 2000        #⚠️
+max_itrtn_for_learning = 2000        #⚠️
+min_batch_size_for_learning = 1
+max_batch_size_for_learning = 1
+min_param_for_learning = 1
+max_param_for_learning = 1
+PER = False 
 
+beta = 0.1
+min_itrtn_for_planning = 1
+max_itrtn_for_planning = 50        
+
+episode_for_training = 100000
+episode_for_validation = 10
+episode_for_averaging = 50
+buffer_limit = 50000
+render_for_human = False
+
+# -----------------------
 
 
 
