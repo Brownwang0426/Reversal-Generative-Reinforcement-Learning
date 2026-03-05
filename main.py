8b8c956567601f7c74f469315c2924ab4bce7416
Po-Lung Wang <Brownwang0426@gmail.com>
2026-03-05 23:39:13 +0800

    w

--- main.py
+++ main.py
@@ -134,55 +134,6 @@ render_for_human = False
 
 
 
-# -----------------------
-
-game_name = "LunarLander-v3"         #⚠️
-max_steps_for_each_episode = 200     #⚠️
-seed = None                          #⚠️
-load_pretrained_model = True
-ensemble_size = 5                    #◀️
-state_size =  900                    #⚠️
-action_size = 4                      #⚠️
-reward_size = 100                    #⚠️
-feature_size = 1000                  #⚠️
-history_size = 200                   #⚠️
-future_size = 150                    #⚠️ 
-frame_skip = 1                       #⚠️ 
-pos_skip = 1                         #⚠️
-neural_type = 'td'                   #⚠️
-num_layers = 5                       #⚠️
-num_heads = 10                       #⚠️
-num_experts = 1                      #⚠️
-moe_top_k = 1                        #⚠️
-
-init = "xavier_normal"
-opti = 'sgd'
-loss = 'mean_squared_error'
-bias = False
-drop_rate = 0.01
-alpha = 0.1
-L2_lambda = 0                 
-grad_clip_value = 1.0
-magnitude_for_learning = 5           #⚠️
-batch_size_old = 25                  #⚠️ 
-PER = False
-
-beta = 0.01
-min_itrtn_for_planning = 1           #⚠️
-max_itrtn_for_planning = 100         #⚠️     
-
-episode_for_training = 100000
-episode_for_validation = 10
-episode_for_averaging = 30
-buffer_limit = 100000
-render_for_human = False
-
-
-
-
-
-
-
 
 
 
@@ -252,6 +203,55 @@ reward_size = 100                    #⚠️
 feature_size = 500                   #⚠️
 history_size = 1000                  #⚠️
 future_size = 100                    #⚠️
+frame_skip = 1                       #⚠️ 
+pos_skip = 1                         #⚠️
+neural_type = 'td'                   #⚠️
+num_layers = 5                       #⚠️
+num_heads = 10                       #⚠️
+num_experts = 1                      #⚠️
+moe_top_k = 1                        #⚠️
+
+init = "xavier_normal"
+opti = 'sgd'
+loss = 'mean_squared_error'
+bias = False
+drop_rate = 0.01
+alpha = 0.1
+L2_lambda = 0                 
+grad_clip_value = 1.0
+magnitude_for_learning = 5           #⚠️
+batch_size_old = 25                  #⚠️ 
+PER = False
+
+beta = 0.01
+min_itrtn_for_planning = 1           #⚠️
+max_itrtn_for_planning = 100         #⚠️     
+
+episode_for_training = 100000
+episode_for_validation = 10
+episode_for_averaging = 30
+buffer_limit = 100000
+render_for_human = False
+
+
+
+
+
+
+
+# -----------------------
+
+game_name = "LunarLander-v3"         #⚠️
+max_steps_for_each_episode = 200     #⚠️
+seed = None                          #⚠️
+load_pretrained_model = True
+ensemble_size = 5                    #◀️
+state_size =  900                    #⚠️
+action_size = 4                      #⚠️
+reward_size = 100                    #⚠️
+feature_size = 1000                  #⚠️
+history_size = 200                   #⚠️
+future_size = 150                    #⚠️ 
 frame_skip = 1                       #⚠️ 
 pos_skip = 1                         #⚠️
 neural_type = 'td'                   #⚠️
