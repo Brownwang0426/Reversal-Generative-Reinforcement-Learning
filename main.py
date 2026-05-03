9913f654972a6b738eb47c2a5fe2312e0a8c01d5
Po-Lung Wang <Brownwang0426@gmail.com>
2026-05-03 20:12:47 +0800

    =w=

--- main.py
+++ main.py
@@ -140,6 +140,51 @@ render_for_human = False
 
 # -----------------------
 
+
+game_name = 'CartPole-v1'            #⚠️
+max_steps_for_each_episode = 1000    #⚠️
+seed = None                          #⚠️
+load_pretrained_model = True
+ensemble_size = 5                    #◀️
+reward_size = 100                    #⚠️
+state_size =  700                    #⚠️
+action_size = 2                      #⚠️
+feature_size = 850                   #⚠️
+history_size = 5                     #⚠️
+future_size = 25                     #⚠️
+frame_skip = 1                       #⚠️ 
+neural_type = 'td_chain'             #⚠️
+num_layers = 5                       
+num_heads = 10                       
+num_experts = 1                      
+moe_top_k = 1                        
+
+init = "xavier_normal"
+opti = 'sgd'
+loss = 'mean_squared_error'
+bias = False
+drop_rate = 0.01
+alpha = 0.1
+L2_lambda = 0                 
+grad_clip_value = 1.0
+itrtn_for_learning = 1500            #⚠️
+PER = False
+
+beta = 0.1
+min_itrtn_for_planning = 1           #⚠️
+max_itrtn_for_planning = 25          #⚠️     
+min_param_for_planning = 0.0         #⚠️
+max_param_for_planning = 0.01        #⚠️
+
+episode_for_training = 100000
+episode_for_validation = 1
+episode_for_averaging = 10
+buffer_limit = 100000                #⚠️
+render_for_human = False
+
+
+# -----------------------
+
 game_name = "LunarLander-v3"         #⚠️
 max_steps_for_each_episode = 200     #⚠️
 seed = None                          #⚠️
@@ -182,51 +227,6 @@ buffer_limit = 100000                #⚠️
 render_for_human = False
 
 
-
-
-# -----------------------
-
-
-game_name = 'CartPole-v1'            #⚠️
-max_steps_for_each_episode = 1000    #⚠️
-seed = None                          #⚠️
-load_pretrained_model = True
-ensemble_size = 5                    #◀️
-reward_size = 100                    #⚠️
-state_size =  700                    #⚠️
-action_size = 2                      #⚠️
-feature_size = 850                   #⚠️
-history_size = 5                     #⚠️
-future_size = 25                     #⚠️
-frame_skip = 1                       #⚠️ 
-neural_type = 'td_chain'             #⚠️
-num_layers = 5                       
-num_heads = 10                       
-num_experts = 1                      
-moe_top_k = 1                        
-
-init = "xavier_normal"
-opti = 'sgd'
-loss = 'mean_squared_error'
-bias = False
-drop_rate = 0.01
-alpha = 0.1
-L2_lambda = 0                 
-grad_clip_value = 1.0
-itrtn_for_learning = 1500            #⚠️
-PER = False
-
-beta = 0.1
-min_itrtn_for_planning = 1           #⚠️
-max_itrtn_for_planning = 25          #⚠️     
-min_param_for_planning = 0.0         #⚠️
-max_param_for_planning = 0.01        #⚠️
-
-episode_for_training = 100000
-episode_for_validation = 1
-episode_for_averaging = 10
-buffer_limit = 100000                #⚠️
-render_for_human = False
 
 
 
@@ -477,6 +477,8 @@ for training_episode in tqdm(range(episode_for_training)):
 
         # executing action
         state, reward, done, truncated, info = env.step(action_)
+        if post_done_truncated_counter > 0:
+            reward = 0
         if (render_for_human == True) and (post_done_truncated_counter == 0):
             env.render()
 
